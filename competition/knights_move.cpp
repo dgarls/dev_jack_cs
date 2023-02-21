388da93528900669f17ba5670d80d8ea722bc4cf
dgarls <dgarls.business@gmail.com>
2023-02-21 08:45:48 -0700

    new file:   .vscode/settings.json 	new file:   competition/knights_move.cpp

---
+++ competition/knights_move.cpp
@@ -0,0 +1,22 @@
+/*
+    The first (and only) line of input is the location of the knight in algebraic notation. 
+    It is guaranteed that it will be a valid square on an 8 x 8 chessboard.
+
+    Output all of the knight's possible moves in a lexicographic order
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string pos;
+    getline(cin, pos);
+
+    
+
+    return 0;
+}
