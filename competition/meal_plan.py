fbf8ae199cd172624d21b2a83ce859e6f307af04
dgarls <dgarls.business@gmail.com>
2023-03-10 15:13:11 -0700

    new file:   competition/meal_plan.py 	new file:   competition/tic_tac_toe_ai.py

---
+++ competition/meal_plan.py
@@ -0,0 +1,25 @@
+people = int(input())
+
+marble = {
+    'meals': 19,
+    'munch': 200
+}
+
+marble_plus = {
+    'meals': 19,
+    'munch': 350
+}
+
+quartz = {
+    'meals': 14,
+    'munch': 200
+}
+
+quartz_plus = {
+    'meals': 14,
+    'munch': 350
+}
+
+for p in range(people):
+    person = input().split()
+    
