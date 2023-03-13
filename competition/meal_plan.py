b8032f010885215c4d2f5fa00b17730f95174453
dgarls <dgarls.business@gmail.com>
2023-03-13 08:42:33 -0600

    modified:   competition/meal_plan.py 	new file:   competition/premiere-league.py

--- competition/meal_plan.py
+++ competition/meal_plan.py
@@ -22,4 +22,22 @@ quartz_plus = {
 
 for p in range(people):
     person = input().split()
-    
+    if person[1] == "Marble+":
+        plan = marble_plus
+    elif person[1] == "Marble":
+        plan = marble
+    elif person[1] == "Quartz":
+        plan = quartz
+    else:
+        plan = quartz_plus
+    swipes_left = plan.get("meals") - int(person[2])
+    money_left = round(plan.get("munch") - float(person[3]), 2)
+    if swipes_left > 0 and money_left > 0:
+        custom = "Use meal swipe or munch money"
+    elif swipes_left > 0 and money_left == 0:
+        custom = "Use meal swipe"
+    elif swipes_left == 0 and money_left > 0:
+        custom = "Use munch money"
+    else:
+        custom = "Go to Downtown Golden!"
+    print(f"{person[0]} {swipes_left} {money_left} {custom}")
